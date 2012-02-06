50e7ec4086842bfca1ec8f8d1ea31b55b2fe3ede
Matt Kennedy <mattpk90@gmail.com>
2012-02-06 23:18:21 +0000

    jquery data() and localstorage tested but not working, exiled and graveyard features working.

--- xmlscript.php
+++ xmlscript.php
@@ -1,15 +1,15 @@
 <?php
-$conn=mysql_connect("https://aquarius.solent.ac.uk/","8kennm51","hiogro");
+$conn=mysql_connect("Aquarius","8kennm51","hiogro");
 
 mysql_select_db("8kennm51");
 
 $a = mysql_query("select * from cards");
 
 while($row = mysql_fetch_array($a))
-  {
-  echo $row['id'] . " " . $row['name'];
-  echo "<br />";
-  }
+{
+	echo $row[id]."&nbsp;".$row[name];
+	echo "<br />";
+}
 
 mysql_close($conn);
 
